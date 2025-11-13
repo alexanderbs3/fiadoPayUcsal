be4e7d20b5dce918e6fd2a50198c3e227f588f87
@aliceslacerda <alice.lacerda@ucsal.edu.br>
2025-11-13 07:51:24 -0300

    Criação de classe PaymentHandlerRegistry para registrar handlers e regras antifraude.

---
+++ src/main/java/edu/ucsal/fiadopay/registry/PaymentHandlerRegistry.java
@@ -0,0 +1,50 @@
+package edu.ucsal.fiadopay.registry;
+
+import edu.ucsal.fiadopay.annotations.AntiFraud;
+import edu.ucsal.fiadopay.annotations.PaymentMethod;
+import edu.ucsal.fiadopay.handler.AntiFraudRule;
+import edu.ucsal.fiadopay.handler.PaymentHandler;
+import jakarta.annotation.PostConstruct;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PaymentHandlerRegistry {
+
+    private final Map<String, PaymentHandler> handlers = new HashMap<>();
+    private final List<AntiFraudRule> fraudRules = new ArrayList<>();
+
+@Autowired
+private ApplicationContext ctx;
+
+@PostConstruct
+ public void init(){
+   ext.getBeansOfType(PaymentHandler.class).values().forEach(handler ->{
+       PaymentMethod ann = handler.getClass().getAnnotation(PaymentMethod.class);
+      if (ann != null){
+          handlers.put(ann.value().toUpperCase(), handler);
+      }
+    });
+
+ctx.getBeansOfType(AntiFraudRule.class).values().forEach(rule ->{
+   AntiFraud ann = rule.getClass().getAnnotation(AntiFraud.class);
+   if (ann != null) {
+       fraudRules.add(rule);
+   }
+});
+}
+
+public PaymentHandler gatHandler(String method){
+    return handlers.get(method.toUpperCase());
+}
+
+public List<AntiFraudRule> getFraudRules() {
+    return fraudRules;
+}
+}
